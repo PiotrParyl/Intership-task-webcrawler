23fa5bdada5479aeb1d60d549804991251d234d9
PiotrParyl <piotrekzrydek@gmail.com>
2022-11-03 17:16:37 +0100

    siema

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+
+
+def siema():
+
+    pass
+
+
+
+def main_loop():
+    while True:
+
+        url = input("Podaj URL: ")
+
+        if url:
+            print(f"Twój URL to: {url}")
+        else:
+            print("nie podałeś URL")
+
+
+
+
+main_loop()
